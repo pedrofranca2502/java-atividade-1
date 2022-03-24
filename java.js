ab69217071209bb317e3f0d6186c37cadec6bfaa
pedrofranca2502 <pedrofranca250204@gmail.com>
2022-03-24 19:14:56 -0300

    codigo completo

---
+++ java.js
@@ -0,0 +1,17 @@
+
+function calcular(){
+  var semaforo=prompt("DIGITE A COR DO SEMAFORO")
+ 
+
+    if (semaforo == "vermelho"){
+         alert ("PARE")
+    }else if (semaforo == "amarelo"){
+         alert("ATENÇAO")
+    }else{
+         alert("PASSE")
+    }
+    
+} 
+
+
+
